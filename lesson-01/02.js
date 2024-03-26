de15aa1b720fbcaa9d41702d3e25d3d178721849
AntonS <solukov.anton@gmail.com>
2024-03-26 19:51:11 +0300

    1

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,6 +1,6 @@
 let number1 = Math.floor(Math.random() * 100)
-if (number1%2===0){
-    console.log(number1,'четное число')
-}else {
+if (number1%2){
     console.log(number1,'нечетное число')
+}else {
+    console.log(number1,'четное число')
 }
